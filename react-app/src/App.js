a9c14e2b23bc53ab125eb256f4449dcb616f8a33
aryanzero <chauhanaryan2000@gmail.com>
2024-06-03 14:05:52 -0400

    added new changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,9 +4,31 @@ import {ethers} from 'ethers';
 import {contractAbi, contractAddress} from './Constant/constant';
 import Login from './Components/Login';
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+  async function connectToMetamask() {
+    if (window.ethereum) {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(provider);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        console.log("Metamask Connected : " + address);
+        setIsConnected(true);
+        // canVote();
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      console.error("Metamask is not detected in the browser")
+    }
+  }
   return (
     <div className="App">
-      <h1> "hello ji"</h1>
+      <Login connectWallet= {connectToMetamask}/>
     </div>
   );
 }
