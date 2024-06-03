0a610e5cae516406100b3f1cca27b7220def68bc
aryanzero <chauhanaryan2000@gmail.com>
2024-06-03 13:44:32 -0400

    made changes

---
+++ react-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import {ethers} from 'ethers';
+import {contractAbi, contractAddress} from './Constant/constant';
+import Login from './Components/Login';
+function App() {
+  return (
+    <div className="App">
+      <h1> "hello ji"</h1>
+    </div>
+  );
+}
+
+export default App;
