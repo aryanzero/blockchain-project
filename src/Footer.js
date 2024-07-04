01ed635971c2be2a428001baf7a3f1e8e58cc158
AnshAggarwalMAC <51926978+anshaggarwal28@users.noreply.github.com>
2024-07-04 10:20:31 -0400

    updated header and footer functionality
    javascript

--- src/Footer.js
+++ src/Footer.js
@@ -3,30 +3,41 @@ import { Box, Typography, Container, Link } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const FooterContainer = styled(Box)({
-  backgroundColor: '#1e1e1e',
-  padding: '20px 0',
+  backgroundColor: '#0d47a1', // Changed to a vibrant blue color
+  padding: '40px 0', // Increased padding for better spacing
   marginTop: 'auto',
   textAlign: 'center',
   color: '#fff',
+  borderTop: '5px solid #f44336', // Added a border to separate footer from content
+});
+
+const FooterLink = styled(Link)({
+  textDecoration: 'none',
+  margin: '0 15px',
+  color: '#fff',
+  fontFamily: 'Poppins',
+  '&:hover': {
+    color: '#f44336',
+  },
 });
 
 const Footer = () => {
   return (
     <FooterContainer>
       <Container>
-        <Typography variant="body1" style={{ fontFamily: 'Poppins' }}>
+        <Typography variant="body1" style={{ fontFamily: 'Poppins', fontWeight: '500' }}>
           &copy; {new Date().getFullYear()} VoteGuard. All rights reserved.
         </Typography>
         <Typography variant="body2" style={{ fontFamily: 'Poppins', marginTop: '10px' }}>
-          <Link href="/privacy" target="_blank" color="inherit" style={{ textDecoration: 'none', marginRight: '15px' }}>
+          <FooterLink href="/privacy" target="_blank">
             Privacy Policy
-          </Link>
-          <Link href="/terms" target="_blank" color="inherit" style={{ textDecoration: 'none', marginRight: '15px' }}>
+          </FooterLink>
+          <FooterLink href="/terms" target="_blank">
             Terms of Service
-          </Link>
-          <Link href="https://github.com/aryanzero/blockchain-project" target="_blank" color="inherit" style={{ textDecoration: 'none' }}>
+          </FooterLink>
+          <FooterLink href="https://github.com/aryanzero/blockchain-project" target="_blank">
             Voteguard
-          </Link>
+          </FooterLink>
         </Typography>
       </Container>
     </FooterContainer>
@@ -34,3 +45,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
